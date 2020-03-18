ee33a4e5d8b0b98a8c64ec437c7437f2a94c8e8c
adrien <adrien.pusel@gmail.com>
2020-03-18 10:32:52 +0100

    indentation and set all package to latest

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Comment from './Comment';
-import AddComment from './AddComment';
+import React from "react";
+import AddComment from "./AddComment";
+import Comment from "./Comment";
 
-const Comments = ({ comments, onCreate }) => {
-  return (
-    <section className="Comments">
-      <AddComment onCreate={onCreate} />
-      {comments.map(comment => <Comment {...comment} key={comment.id} />)}
-    </section>
-  )
-}
+const Comments = ( { comments, onCreate } ) => {
+	return (
+			<section className="Comments">
+				<AddComment onCreate={ onCreate }/>
+				{ comments.map( comment => <Comment { ...comment }
+																						key={ comment.id }/> ) }
+			</section>
+	);
+};
 
 export default Comments;
